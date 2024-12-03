f928f459d4ad8819c487ba2e401880df5d9d891f
parmsam <parmartsam@gmail.com>
2024-12-02 21:10:44 -0500

    add utility funcs

---
+++ aoc_funcs.py
@@ -0,0 +1,16 @@
+%%writefile aoc_funcs.py
+
+def read_aoc_input(data: str, data_type: str = 'string'):
+    assert isinstance(data, str), f'Invalid data type: {type(data)}. Must be str.'
+    assert data_type in ['string', 'int'], f'Invalid type: {type}. Must be int or char. '
+    
+    data = data.splitlines()
+    
+    if data_type == 'string':
+        data = [list(i) for i in data]
+        pass
+    elif data_type == 'int':
+        data = [i.split() for i in data]
+        data = [[int(x) for x in row] for row in data]
+    
+    return data
